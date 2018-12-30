0e1541872d2a9b274da9c361f2a04e456968d24f
pacson <1993292>
2018-12-30 21:04:25 +0100

    removing Selenium project name from the imported module

--- Demo/Pytest/test_sample.py
+++ Demo/Pytest/test_sample.py
@@ -4,9 +4,9 @@ import pytest
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 # from POMDemo.Pages.homePage import HomePage
-from Selenium.POMDemo.Pages.LoginPage import LoginPage
+from POMDemo.Pages.LoginPage import LoginPage
 
 
 class TestSample():
